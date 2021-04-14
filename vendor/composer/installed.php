812fa92005a353cb49bd86b74e882c6e1505243e
Ilya <lord123147@gmail.com>
2021-04-14 10:47:29 +0700

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,557 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'mediawiki/core',
+  ),
+  'versions' => 
+  array (
+    'composer/semver' => 
+    array (
+      'pretty_version' => '3.2.4',
+      'version' => '3.2.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a02fdf930a3c1c3ed3a49b5f63859c0c20e10464',
+    ),
+    'cssjanus/cssjanus' => 
+    array (
+      'pretty_version' => 'v1.3.0',
+      'version' => '1.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '93db9fb39c2f9bcf287715ae254c088b14a77799',
+    ),
+    'guzzlehttp/guzzle' => 
+    array (
+      'pretty_version' => '7.2.0',
+      'version' => '7.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0aa74dfb41ae110835923ef10a9d803a22d50e79',
+    ),
+    'guzzlehttp/promises' => 
+    array (
+      'pretty_version' => '1.4.1',
+      'version' => '1.4.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8e7d04f1f6450fef59366c399cfad4b9383aa30d',
+    ),
+    'guzzlehttp/psr7' => 
+    array (
+      'pretty_version' => '1.8.1',
+      'version' => '1.8.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '35ea11d335fd638b5882ff1725228b3d35496ab1',
+    ),
+    'liuggio/statsd-php-client' => 
+    array (
+      'pretty_version' => 'v1.0.18',
+      'version' => '1.0.18.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c42e6d6687b7b2d7683186ec7f4f03351cc3dbca',
+    ),
+    'mediawiki/core' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'mediawiki/mediawiki' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.37.0-alpha',
+      ),
+    ),
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '2.2.0',
+      'version' => '2.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1cb1cde8e8dd0f70cc0fe51354a59acad9302084',
+    ),
+    'oojs/oojs-ui' => 
+    array (
+      'pretty_version' => 'v0.41.3',
+      'version' => '0.41.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0034158b0a4ed728908a6e564a7589c602e4174e',
+    ),
+    'pear/console_getopt' => 
+    array (
+      'pretty_version' => 'v1.4.3',
+      'version' => '1.4.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a41f8d3e668987609178c7c4a9fe48fecac53fa0',
+    ),
+    'pear/mail' => 
+    array (
+      'pretty_version' => 'v1.4.1',
+      'version' => '1.4.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9609ed5e42ac5b221dfd9af85de005c59d418ee7',
+    ),
+    'pear/mail_mime' => 
+    array (
+      'pretty_version' => '1.10.9',
+      'version' => '1.10.9.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1e7ae4e5258b6c0d385a8e76add567934245d38d',
+    ),
+    'pear/net_smtp' => 
+    array (
+      'pretty_version' => '1.9.2',
+      'version' => '1.9.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '18b5dbaa049bee5f6d02c9a4318db02472e2c922',
+    ),
+    'pear/net_socket' => 
+    array (
+      'pretty_version' => 'v1.2.2',
+      'version' => '1.2.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bbe6a12bb4f7059dba161f6ddd43f369c0ec8d09',
+    ),
+    'pear/net_url2' => 
+    array (
+      'pretty_version' => 'v2.2.2',
+      'version' => '2.2.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '07fd055820dbf466ee3990abe96d0e40a8791f9d',
+    ),
+    'pear/pear-core-minimal' => 
+    array (
+      'pretty_version' => 'v1.10.10',
+      'version' => '1.10.10.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '625a3c429d9b2c1546438679074cac1b089116a7',
+    ),
+    'pear/pear_exception' => 
+    array (
+      'pretty_version' => 'v1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b14fbe2ddb0b9f94f5b24cf08783d599f776fff0',
+    ),
+    'pleonasm/bloom-filter' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4a3292c9f83a778c44271bf4e4f6be1204b87f7b',
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => '1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b7ce3b176482dbbc1245ebf52b181af44c2cf55f',
+    ),
+    'psr/container-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0.0',
+      ),
+    ),
+    'psr/http-client' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2dfb5f6c5eff0e91e20e913f8c5452ed95b86621',
+    ),
+    'psr/http-client-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6561bf28d520154e4b0ec72be95418abe6d9363',
+    ),
+    'psr/http-message-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.3',
+      'version' => '1.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f73288fd15629204f9d42b7055f72dacbe811fc',
+    ),
+    'psr/log-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0.0',
+      ),
+    ),
+    'ralouphie/getallheaders' => 
+    array (
+      'pretty_version' => '3.0.3',
+      'version' => '3.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+    ),
+    'rsky/pear-core-min' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v1.10.10',
+      ),
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '1.99',
+      ),
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '1.99',
+      ),
+    ),
+    'wikimedia/assert' => 
+    array (
+      'pretty_version' => 'v0.5.0',
+      'version' => '0.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd8e97f15b8f3d3799899e3d56452d34511be5764',
+    ),
+    'wikimedia/at-ease' => 
+    array (
+      'pretty_version' => 'v2.1.0',
+      'version' => '2.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e8ebaa7bb7c8a8395481a05f6dc4deaceab11c33',
+    ),
+    'wikimedia/base-convert' => 
+    array (
+      'pretty_version' => 'v2.0.1',
+      'version' => '2.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '449f0d0237cf1e0e71faec90680c88d4af6e711d',
+    ),
+    'wikimedia/cdb' => 
+    array (
+      'pretty_version' => '1.4.1',
+      'version' => '1.4.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd49d96232f045311dc1f21bf0d7e40adf82a1f1d',
+    ),
+    'wikimedia/cldr-plural-rule-parser' => 
+    array (
+      'pretty_version' => 'v2.0.0',
+      'version' => '2.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '83d78cb8018d5c0f66fd6d0efff6a8ae2de92d36',
+    ),
+    'wikimedia/common-passwords' => 
+    array (
+      'pretty_version' => 'v0.3.0',
+      'version' => '0.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5b51a88a27e17f485ba1295ee6916620686cd5a5',
+    ),
+    'wikimedia/composer-merge-plugin' => 
+    array (
+      'pretty_version' => 'v2.0.1',
+      'version' => '2.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8ca2ed8ab97c8ebce6b39d9943e9909bb4f18912',
+    ),
+    'wikimedia/html-formatter' => 
+    array (
+      'pretty_version' => '3.0.1',
+      'version' => '3.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f18622f3384b9b7fed185bff2a46594aec92fa47',
+    ),
+    'wikimedia/ip-set' => 
+    array (
+      'pretty_version' => '3.0.0',
+      'version' => '3.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4efe81f0ffb907a60778a72faf6ede17bb490081',
+    ),
+    'wikimedia/ip-utils' => 
+    array (
+      'pretty_version' => '3.0.2',
+      'version' => '3.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ba5db9991418c9b00c4ef7d327c32bf595755d02',
+    ),
+    'wikimedia/less.php' => 
+    array (
+      'pretty_version' => 'v3.1.0',
+      'version' => '3.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a486d78b9bd16b72f237fc6093aa56d69ce8bd13',
+    ),
+    'wikimedia/minify' => 
+    array (
+      'pretty_version' => '2.2.1',
+      'version' => '2.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5fea8611bfcebe23eeba1b1eef21d3ccd8376302',
+    ),
+    'wikimedia/object-factory' => 
+    array (
+      'pretty_version' => 'v3.0.0',
+      'version' => '3.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0d2c4efc08e80485e5f7b1665fe473375e242017',
+    ),
+    'wikimedia/parsoid' => 
+    array (
+      'pretty_version' => 'v0.13.0-a32',
+      'version' => '0.13.0.0-alpha32',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '42b1091250163d046f92d2217e97817bc64ed1a6',
+    ),
+    'wikimedia/php-session-serializer' => 
+    array (
+      'pretty_version' => 'v2.0.0',
+      'version' => '2.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '99e7e926f1b61f71623d517fe38d9eec8618c59d',
+    ),
+    'wikimedia/purtle' => 
+    array (
+      'pretty_version' => 'v1.0.7',
+      'version' => '1.0.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '569064c065caae036cd5a685574b6f96f05cfe33',
+    ),
+    'wikimedia/relpath' => 
+    array (
+      'pretty_version' => '2.1.1',
+      'version' => '2.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '35e701ff16abf461bb8676a9d9177f86fa0b2c94',
+    ),
+    'wikimedia/remex-html' => 
+    array (
+      'pretty_version' => '2.2.2',
+      'version' => '2.2.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3f55e728403fe52445c3aaab20f5e4be8e2b282b',
+    ),
+    'wikimedia/request-timeout' => 
+    array (
+      'pretty_version' => '1.1.0',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8828e701fbc4e6090e3a5baac2ffc14730f099ad',
+    ),
+    'wikimedia/running-stat' => 
+    array (
+      'pretty_version' => 'v1.2.1',
+      'version' => '1.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '60eebada7cc64b7073d90e7d4bab00efaafa0ba9',
+    ),
+    'wikimedia/scoped-callback' => 
+    array (
+      'pretty_version' => 'v3.0.0',
+      'version' => '3.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0a480d9a9772634697c77598726cf24606597bd0',
+    ),
+    'wikimedia/services' => 
+    array (
+      'pretty_version' => '2.0.1',
+      'version' => '2.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5ef69a8a8b0d2ea115d08469bdab468f58fac820',
+    ),
+    'wikimedia/shellbox' => 
+    array (
+      'pretty_version' => '1.0.4',
+      'version' => '1.0.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd7ad649b7443d86235d7f179d205f9b9476c9c63',
+    ),
+    'wikimedia/timestamp' => 
+    array (
+      'pretty_version' => 'v3.0.0',
+      'version' => '3.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '42ce5586d2189826e28ebcf4cedc96226b6d77e2',
+    ),
+    'wikimedia/utfnormal' => 
+    array (
+      'pretty_version' => '3.0.2',
+      'version' => '3.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e690d29487a6ee346bcf4cbf5a6fd89170fa2306',
+    ),
+    'wikimedia/wait-condition-loop' => 
+    array (
+      'pretty_version' => 'v2.0.1',
+      'version' => '2.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '44637dcee19765c4887cd3cc0fd97db0eabd7688',
+    ),
+    'wikimedia/wikipeg' => 
+    array (
+      'pretty_version' => '2.0.5',
+      'version' => '2.0.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'df47c20859c80923d08ce906c5c7412161ef052f',
+    ),
+    'wikimedia/wrappedstring' => 
+    array (
+      'pretty_version' => 'v3.2.0',
+      'version' => '3.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '11b01a2286fd377cb15c2f5561e10f56d58f37a9',
+    ),
+    'wikimedia/xmp-reader' => 
+    array (
+      'pretty_version' => '0.8.1',
+      'version' => '0.8.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4fc577e28e09eec165b64ed74ce878ba4ea45a35',
+    ),
+    'wikimedia/zest-css' => 
+    array (
+      'pretty_version' => '1.1.5',
+      'version' => '1.1.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bc3346fcf2a93f02a8a6697d98c305c14ef1869f',
+    ),
+    'zordius/lightncandy' => 
+    array (
+      'pretty_version' => 'v1.2.5',
+      'version' => '1.2.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '37aa381e0f27d411a630062070c7a5a2174c62e7',
+    ),
+  ),
+);
